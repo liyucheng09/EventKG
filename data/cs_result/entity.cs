74e57d000f58062e3030c64913221a51dd954784
yucheng <yucheng.li@surrey.ac.uk>
2022-01-19 23:14:22 +0000

    add result

---
+++ data/cs_result/entity.cs
@@ -0,0 +1,3 @@
+:Entity_EDL_0000001	type	https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/SeedlingOntology#Person	1.000000
+:Entity_EDL_0000001	canonical_mention	"boar"	test.txt:5-6	0.000
+:Entity_EDL_0000001	mention	"boar"	test.txt:5-6	0.000
